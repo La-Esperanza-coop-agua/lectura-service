0d80567507c501f2c2c123772ae457e89ebc7daf
Crosral <jeral.melillancachavez@gmail.com>
2026-05-18 12:12:09 -0400

    Add files via upload

---
+++ lectura/src/main/java/cl/esperanza/lectura/model/Lecturas.java
@@ -0,0 +1,31 @@
+package cl.esperanza.lectura.model;
+
+
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+
+
+public class Lecturas {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+
+
+}
---
+++ lectura/src/test/java/cl/esperanza/lectura/LecturaApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.esperanza.lectura;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LecturaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
