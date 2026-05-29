b519fb4e9aac6eed3b3c994ed1b07000ac80dcc4
TheGhouzt <elrandom989@gmail.com>
2026-05-28 21:18:47 -0400

    Delete lectura directory

--- lectura/src/main/java/cl/esperanza/lectura/exception/LecturaInvalidaException.java
+++
@@ -1,9 +0,0 @@
-package cl.esperanza.lectura.exception;
-
-public class LecturaInvalidaException 
-        extends RuntimeException {
-
-    public LecturaInvalidaException(String mensaje) {
-        super(mensaje);
-    }
-}
