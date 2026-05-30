bd712205589e138b38d46bcfd12b99d9a8a78119
CyCklop <ignacioandresflores06@gmail.com>
2026-05-29 22:13:00 -0400

    last fix

--- src/main/java/cl/esperanza/lectura/controller/LecturaController.java
+++ src/main/java/cl/esperanza/lectura/controller/LecturaController.java
@@ -1,13 +1,20 @@
 package cl.esperanza.lectura.controller;
 
-import jakarta.validation.Valid;
+import java.util.List;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import cl.esperanza.lectura.dto.CreateLecturaRequest;
 import cl.esperanza.lectura.model.Lectura;
 import cl.esperanza.lectura.service.LecturaService;
-import cl.esperanza.lectura.dto.CreateLecturaRequest;
-import java.util.List;
+import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/api/v1/lectura")
@@ -22,6 +29,13 @@ public class LecturaController {
     @GetMapping("/socio/{run}")
     public ResponseEntity<List<Lectura>> getLecturasPorSocio(@PathVariable String run) {
         return ResponseEntity.ok(lecturaService.obtenerLecturasPorSocio(run));
+    }
+
+    // Endpoint que usara el micro de facturacion
+    @GetMapping("/socio/{run}/periodo/{periodo}")
+    public ResponseEntity<Lectura> getLecturaPorPeriodo(@PathVariable String run, @PathVariable String periodo){
+        Lectura lectura = lecturaService.obtenerLecturaExacta(run, periodo);
+        return ResponseEntity.ok(lectura);
     }
 
     @PostMapping("/registrar")
